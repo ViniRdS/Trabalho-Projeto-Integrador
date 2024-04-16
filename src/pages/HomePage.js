1de712d4de91349d44ceb4937896945ed7f200ac
AndriFarias <andrielly.farias21@gmail.com>
2024-04-16 08:42:42 -0400

    style: Ajustando o estilo da pagina home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import NewTitle from "../components/newTitle.component";
+import SubContainer from "../components/subContainer.component";
 
 
-export default function Home(){
-
-    return(
-        <NewTitle/>
+export default function Home() {
+    return (
+        <React.Fragment>
+            <NewTitle/>
+            <SubContainer/>
+        </React.Fragment>
     )
 }
